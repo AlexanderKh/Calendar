c7c99fa603a1ebd0a5d8e6a9d474c5f474d15b46
Alexander Khomchenko <alexanderkh@interlink-ua.com>
2015-05-20 09:15:25 +0300

    Refactor - stage 1

--- src/main/java/Alex/ImprovedCalendar.java
+++ src/main/java/Alex/ImprovedCalendar.java
@@ -1,37 +1,34 @@
 package Alex;
 
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Scanner;
 
 class ImprovedCalendar{
+	public static final int DAYS_IN_WEEK = 7;
+	public static final int MONTHS_IN_YEAR = 12;
+
 	public static void main(String[] args) {
 		Calendar calendar = Calendar.getInstance();
-		ArrayList<Integer> date;
 		try{
-		if (args.length == 0)
-			date = interactiveInput();
-		else
-			date = parseConsoleInput(args);
+			if (args.length == 0)
+				interactiveInput(calendar);
+			else
+				parseConsoleInput(calendar, args);
 		}catch(Exception e){
 			System.out.println("Error");
 			return;
 		}
-		showCalendar(calendar, date);
+		showCalendar(calendar);
 	}
 
-	public static void showCalendar(Calendar calendar, ArrayList<Integer> date){
-		int year = date.get(0);
-		int month = date.get(1);
-		int day = date.get(2);
-		calendar.set(Calendar.YEAR, year);
-		calendar.set(Calendar.MONTH, month);
+	public static void showCalendar(Calendar calendar){
+		int month = calendar.get(Calendar.MONTH);
+		int day = calendar.get(Calendar.DAY_OF_MONTH);
 		calendar.set(Calendar.DAY_OF_MONTH, 1);
-		while(calendar.get(Calendar.DAY_OF_WEEK) > 1)
-			calendar.add(Calendar.DAY_OF_YEAR, -1);
-		System.out.println("\u001B[31mSun\u001B[0m\tMon\tTue\tWed\tThu\tFri\t\u001B[31mSat\u001B[0m ");
-		while ((calendar.get(Calendar.MONTH) + 12) % 11 <= month + 1 && calendar.get(Calendar.YEAR) <= year){
-			for (int j = 0; j < 7; j++){
+		rewindToSunday(calendar);
+		printHeaderColor();
+		while (isCurrentOrPreviousMonth(calendar, month)){
+			for (int j = 0; j < DAYS_IN_WEEK; j++){
 				colorSwitch(calendar, month, day);
 				System.out.print(calendar.get(Calendar.DAY_OF_MONTH) + "\t");
 				calendar.add(Calendar.DAY_OF_YEAR, 1);
@@ -41,16 +38,44 @@ class ImprovedCalendar{
 		System.out.print("\u001B[0m");
 	}
 
+	public static boolean isCurrentOrPreviousMonth(Calendar calendar, int month){
+		int currentMonth = calendar.get(Calendar.MONTH);
+		if (month == currentMonth)
+			return true;
+		if (month - 1 == currentMonth)
+			return true;
+		if (month == 0 && currentMonth == 11)
+			return true;
+		return false;
+	}
+	
+	public static void rewindToSunday(Calendar calendar) {
+		while(calendar.get(Calendar.DAY_OF_WEEK) > 1)
+			calendar.add(Calendar.DAY_OF_YEAR, -1);
+	}
+
+	public static void printHeaderColor() {
+		System.out.println("\u001B[31mSun\u001B[0m\tMon\tTue\tWed\tThu\tFri\t\u001B[31mSat\u001B[0m ");
+	}
+	
+	public static void printHeaderColorless() {
+		System.out.println("Sun\tMon\tTue\tWed\tThu\tFri\tSat");
+	}
+
 	public static void colorSwitch(Calendar calendar, int month, int day){
 		if (calendar.get(Calendar.MONTH) != month){
 					System.out.print("\u001B[33m");
 				}else{
 					System.out.print("\u001B[0m");
-					if (calendar.get(Calendar.DAY_OF_WEEK) == 7 || calendar.get(Calendar.DAY_OF_WEEK) == 1)
+					if (isWeekend(calendar))
 						System.out.print("\u001B[31m");
 					if (calendar.get(Calendar.DAY_OF_MONTH) == day)
 						System.out.print("\u001B[32m");
 				}
+	}
+
+	public static boolean isWeekend(Calendar calendar) {
+		return calendar.get(Calendar.DAY_OF_WEEK) == 7 || calendar.get(Calendar.DAY_OF_WEEK) == 1;
 	}
 
 	public static int parseMonth(String month) throws Exception{
@@ -87,28 +112,25 @@ class ImprovedCalendar{
 		throw new Exception("Month conversion error");
 	}
 
-	public static ArrayList<Integer> interactiveInput() throws Exception{
-		ArrayList<Integer> list = new ArrayList<Integer>();
+	public static void interactiveInput(Calendar calendar) throws Exception{
 		Scanner in = new Scanner(System.in);
 		System.out.print("Input Year: ");
-		list.add(Integer.valueOf(in.next()));
+		calendar.set(Calendar.YEAR, Integer.valueOf(in.next()));
 		System.out.print("Input Month: ");
-		list.add(parseMonth(in.next()));
+		calendar.set(Calendar.MONTH, parseMonth(in.next()));
 		System.out.print("Input Day: ");
 		int day = Integer.valueOf(in.next());
 		while (day > 31 || day < 1){
 			System.out.println("Wrong input, please try again: ");
 			day = Integer.valueOf(in.next());
 		}
-		list.add(day);
-		return list;
+		calendar.set(Calendar.DAY_OF_MONTH, day);
+		in.close();
 	}
 
-	public static ArrayList<Integer> parseConsoleInput(String[] input) throws Exception{
-		ArrayList<Integer> list = new ArrayList<Integer>();
-		list.add(Integer.valueOf(input[0]));
-		list.add(parseMonth(input[1]));
-		list.add(Integer.valueOf(input[2]));
-		return list;
+	public static void parseConsoleInput(Calendar calendar, String[] input) throws Exception{
+		calendar.set(Calendar.YEAR, Integer.valueOf(input[0]));
+		calendar.set(Calendar.MONTH, parseMonth(input[1]));
+		calendar.set(Calendar.DAY_OF_MONTH, Integer.valueOf(input[2]));
 	}
 }
