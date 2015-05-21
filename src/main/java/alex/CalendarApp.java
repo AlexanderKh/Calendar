1f5bcf54565a131b921ab978cb52445196e1b66d
Alexander Khomchenko <alexanderkh@interlink-ua.com>
2015-05-21 16:33:45 +0300

    fixed CalendarApp

--- src/main/java/alex/CalendarApp.java
+++ src/main/java/alex/CalendarApp.java
@@ -6,13 +6,13 @@ public class CalendarApp {
     public static void main(String[] args){
         Calendar date = Calendar.getInstance();
         InputReader inputReader = new InputReader();
-        boolean wirteToHTML = false;
+        boolean writeToHTML = false;
         if (args.length > 2 && args[2].equals("--out")){
             args[3] = args[3].toUpperCase();
-            wirteToHTML = args[3].equals("HTML") ? true : false;
+            writeToHTML = args[3].equals("HTML");
         }
         date.setTime(inputReader.getFirstDayOfMonth(args));
-        MonthCalendar calendar = new MonthCalendar(date,wirteToHTML);
+        MonthCalendar calendar = new MonthCalendar(date,writeToHTML);
         System.out.println(calendar);
     }
 }
